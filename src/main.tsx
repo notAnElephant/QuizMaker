a4a014f9b86c742b9cb924a12b681d8d02219121
Olivér Raisz <raiszoliver@gmail.com>
2025-11-22 17:25:43 +0100

    refact: reorganised Hasura files

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
+import {Analytics} from "@vercel/analytics/next"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import App from './App';
 import './index.css';
 import QuestionView from './components/QuestionView';
-import {QuizProvider} from './context/QuizContext';
 import Settings from "./components/Settings.tsx";
 import Teams from "./components/Teams.tsx";
+import {QuizProvider} from './context/QuizContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
             <QuizProvider>
+                <Analytics/>
                 <Routes>
                     <Route path="/" element={<App/>}/>
                     <Route path="/question/:catIndex/:qIndex" element={<QuestionView/>}/>
